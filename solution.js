9b94b1a04f2f8949d0537e11db45d135a85709d1
Your Name <tpunchihewage@gmail.com>
2022-07-01 09:13:46 +0200

    files added

---
+++ solution.js
@@ -0,0 +1,41 @@
+// section - 1
+
+const snackServings = 15;
+const guests = 6;
+
+console.log(snackServings + guests);
+console.log(snackServings - guests);
+console.log(snackServings * guests);
+console.log(snackServings / guests);
+
+const drinks = 10;
+
+const resultOne = (snackServings * guests) / drinks;
+
+console.log(resultOne);
+
+// section - 2
+
+const monkeys = 15;
+const bananas = 9;
+
+console.log(monkeys / bananas);
+
+const monkeyFights = 20;
+const result2 = (monkeys + bananas) * monkeyFights;
+
+console.log(result2);
+
+let monkeyBusiness = 90;
+
+console.log(++monkeyBusiness);
+
+let boredom = 13;
+
+console.log(--boredom);
+
+const happiness = bananas - monkeys;
+
+console.log(happiness);
+
+console.log(resultOne / result2);
